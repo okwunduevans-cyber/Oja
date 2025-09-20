e77fe3c54c37d7e23b7ef2626dfd5aecd9dc8e54
okwunduevans-cyber <okwunduevans@gmail.com>
2025-09-21 01:32:26 +0200

    Add lazy item import to Compose screens

--- app/src/main/java/com/oja/app/ui/screens/JobsDashboardScreen.kt
+++ app/src/main/java/com/oja/app/ui/screens/JobsDashboardScreen.kt
@@ -3,6 +3,7 @@ package com.oja.app.ui.screens
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.item
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
